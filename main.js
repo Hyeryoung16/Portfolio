91abb07a62cb7eeaaf59de054200e2891d058bf8
Hyeryoung16 <hyeryoung16@gmail.com>
2021-02-18 15:38:05 +0900

    JS : Navbar

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 'user strict';
 
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
 /* Event 등록*/
 /* 'scroll' 될때마다 function()을 실행*/
-document.addEventListener('scroll', function(){
-  var y = window.scrollY;
-})
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  }
+  else{
+    navbar.classList.remove('navbar--dark');
+  }
+});
